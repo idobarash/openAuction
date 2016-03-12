8170bb40778ace6fb7bfd4ca2a291403a0581056
ido <ido.barash@cooladata.com>
2016-03-12 13:19:50 +0200

    template

---
+++ auction-app/src/main/java/viewHandlers/SearchView.java
@@ -0,0 +1,15 @@
+package viewHandlers;
+
+
+public class SearchView {
+
+    private String searchText;
+
+    public String getSearchText() {
+        return searchText;
+    }
+
+    public void setSearchText(String searchText) {
+        this.searchText = searchText;
+    }
+}
