8ae4ecb42f8478d93ff4f3188d7e74943d70bbbe
ido <ido.barash@cooladata.com>
2016-04-03 18:39:07 +0300

    rename

--- auction-app/src/main/java/contorllers/ItemsListController.java
+++ auction-app/src/main/java/contorllers/ItemsListController.java
@@ -2,13 +2,15 @@ package contorllers;
 
 import entity.Item;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import javax.inject.Named;
 import java.util.List;
 
-@Named
-@SessionScoped
-public class ItemsController {
+@RequestScoped
+@ManagedBean(name = "itemsListController")
+public class ItemsListController {
 
     public List<Item> getItems() {
         //return ItemService.getItems();
